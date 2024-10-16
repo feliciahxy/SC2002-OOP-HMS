edea3e91d932e7d6702e1e1049ec9c2b252fc087
chuayhhh <104501891+chuayhhh@users.noreply.github.com>
2024-10-16 22:01:04 +0800

    Schedule, Medication and Doctor skeleton

---
+++ Schedule.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Schedule {
+    private List<Date> availableDates;
+    private List<Time> availableTimes;
+
+    public Schedule(List<Date> availableDates, List<Time> availableTimes) {
+        this.availableDates = availableDates;
+        this.availableTimes = availableTimes;
+    }
+
+    public void updateAvailability(Date date, Time time) {
+        return;
+    }
+
+    public List<Date> getAvailableDates() {
+        return availableDates;
+    }
+
+    public void setAvailableDates(List<Date> availableDates) {
+        this.availableDates = availableDates;
+    }
+
+    public List<Time> getAvailableTimes() {
+        return availableTimes;
+    }
+
+    public void setAvailableTimes(List<Time> availableTimes) {
+        this.availableTimes = availableTimes;
+    }
+}
