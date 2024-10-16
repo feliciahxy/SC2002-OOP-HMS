75650441149f2921fbd2afc3640cde891e04c0af
feliciahxy <felicia.hxy@gmail.com>
2024-10-16 22:18:47 +0800

    completed inventory

---
+++ Inventory.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Inventory {
+    ArrayList<Medication> medicines;
+    public Inventory(ArrayList<Medication> medicines){
+        this.medicines = medicines;
+    }
+    public void updateStock(String medicineID, int quantity){
+
+    }
+    public void submitReplenishmentRequest(String medicineID){
+
+    }
+    public ArrayList<Medication> getMedicines(){
+
+    }
+    public void setMedicines(ArrayList<Medication> medications){
+
+    }
+
+}
