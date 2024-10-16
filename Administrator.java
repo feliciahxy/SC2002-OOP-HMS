e66dd7ab808c348580fc4a5e7b582901600eae30
feliciahxy <felicia.hxy@gmail.com>
2024-10-16 22:06:09 +0800

    added administrator and medical record

---
+++ Administrator.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.time.LocalDate; 
+
+public class Administrator extends Staff {
+    private ArrayList<Staff> staffList;
+    private Inventory inventory;
+
+    public Administrator(String userID, String password, String staffID, String name, LocalDate dateOfHire, String department, String gender, int age, ArrayList<Staff> staffList, Inventory inventory){
+        super(userID, password, staffID, name, dateOfHire, department, gender, age);
+        this.userID = userID;
+        this.password = password;
+        this.role = "Staff";
+        this.staffID = staffID;
+        this.name = name;
+        this.dateOfHire = dateOfHire;
+        this.department = department;
+        this.staffList = staffList;
+        this.inventory = inventory;
+    }
+    public void manageStaff(String staffID, String action){
+        // 
+    }
+    public ArrayList<Staff> viewAppointments(){
+        return 
+    }
+    public void manageInventory(String medicineID, int quantity){
+
+    }
+    public void approveReplenishmentRequest(String requestID){
+
+    }
+    public ArrayList<Staff> getStaffList(){
+
+    }
+    public void setStaffList(ArrayList<Staff> staffList){
+
+    }
+    public Inventory getInventory(){
+        return
+    }
+    public void setInventory(Inventory inventory){
+
+    }
+    public ArrayList<Staff> displayStaff(String filter){
+
+    }
+    public ArrayList<Staff> sortBy(String criteria){
+
+    }
+}
