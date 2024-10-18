28bcc90adf4630dbcf73c38ccfe502ed0140826d
weithai <wtchai6@gmail.com>
2024-10-18 20:54:49 +0800

    ok

---
+++ AppointmentOutcome.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class AppointmentOutcome {
+
+    private String appointmentID;
+    private String serviceType;
+    private Date date;
+    private List<PrescribedMedication> PrescribedMedications;
+    private String notes;
+
+
+    public String getAppointmentID() {
+        return "w";
+    }
+
+    public String getServiceType() {
+        return "w";
+    }
+
+    // public Date getDate() {
+    //     return 
+    // }
+
+    public void setDate(Date date) {
+        return;
+    }
+
+    // public List<PrescribedMedication> getPrescribedMedications() {
+    //     return List<PrescribedMedications>;
+    // }
+
+    public String getNotes() {
+        return "w";
+    }
+
+    public void setNotes(String notes) {
+        return;
+    }
+}
---
+++ PrescribedMedication.java
@@ -0,0 +1,21 @@
+public class PrescribedMedication {
+    
+    private String medicationName;
+    private String medicationStatus;
+
+    public String getMedicationName() {
+        return "w";
+    }
+
+    public void setMedicationName(String name) {
+        return;
+    }
+
+    public String getMedicationStatus() {
+        return "w";
+    }
+
+    public void setMedicationStatus(String status) {
+        return;
+    }
+}
