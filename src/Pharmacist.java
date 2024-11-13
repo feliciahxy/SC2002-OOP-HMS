3ff89c6c1224b654954ce448bec169bfe4b29467
feliciahxy <felicia.hxy@gmail.com>
2024-11-14 00:24:28 +0800

    updated inventory and pharmacist

--- src/Pharmacist.java
+++ src/Pharmacist.java
@@ -1,11 +1,10 @@
 import java.util.*;
 
 public class Pharmacist extends User{
-    private Inventory inventory;
+    Scanner scanner = new Scanner(System.in);
     
-    public Pharmacist(String staffID, String name, String department, String gender, int age, String password, Inventory inventory){
-        super(staffID, name, department, gender, age, password);
-        this.inventory = inventory;
+    public Pharmacist(String staffID, String name, String role, String gender, int age, String password){
+        super(staffID, name, role, gender, age, password);
     }
     
     public AppointmentOutcome findAppointmentOutcomeRecord(String appointmentID, ArrayList<AppointmentOutcome> appointmentOutcomes){
@@ -18,62 +17,77 @@ public class Pharmacist extends User{
         return null;
     }
     
-    public void viewAppointmentOutcomeRecord(AppointmentOutcome appointmentOutcome){
-        ArrayList<PrescribedMedication> PrescribedMedications = appointmentOutcome.getPrescribedMedicationList();
-        System.out.printf("AppointmentID: %s\n", appointmentOutcome.getAppointmentID());
-        for (int i = 0; i<PrescribedMedications.size(); i++){
-            PrescribedMedication med = PrescribedMedications.get(i);
-            System.out.printf("Medication Name: %s, status: %s\n", med.getMedicationName(), med.getMedicationStatus());
-        }
+    public void viewAppointmentOutcomeRecord(ArrayList<AppointmentOutcome> appointmentOutcome){
+        scanner.nextLine();
+        System.out.print("Enter appointment ID: ");
+        String appointmentID = scanner.nextLine(); 
+        AppointmentOutcome apptOutcome = this.findAppointmentOutcomeRecord(appointmentID, appointmentOutcome);
+        if (apptOutcome != null){
+            ArrayList<PrescribedMedication> PrescribedMedications = apptOutcome.getPrescribedMedicationList();
+            System.out.printf("AppointmentID: %s\n", apptOutcome.getAppointmentID());
+            for (int i = 0; i<PrescribedMedications.size(); i++){
+                PrescribedMedication med = PrescribedMedications.get(i);
+                System.out.printf("Medication Name: %s, status: %s\n", med.getMedicationName(), med.getMedicationStatus());
+            }
+        }        
     }
 
-    public void updatePrescription(AppointmentOutcome appointmentOutcome, String medicationName, String status){
-        ArrayList<PrescribedMedication> medlist = appointmentOutcome.getPrescribedMedicationList();
-        boolean found = false;
-        for (int i = 0; i<medlist.size(); i++){
-            PrescribedMedication med = medlist.get(i);
-            if (med.getMedicationName().equals(medicationName)){
-                found = true;
-                if (status.equals("dispensed")){
-                    if(this.inventory.updateStock(medicationName, -1)){
+    // public void updatePrescription(AppointmentOutcome appointmentOutcome, String medicationName, String status){
+    public void updatePrescription(ArrayList<AppointmentOutcome> appointmentOutcomes, Inventory inventory){
+        scanner.nextLine();
+        System.out.print("Enter appointment ID: ");
+        String appointmentID1 = scanner.nextLine(); 
+        AppointmentOutcome appointmentOutcome = this.findAppointmentOutcomeRecord(appointmentID1, appointmentOutcomes);
+        if (appointmentOutcome != null){
+            System.out.print("Enter medication name: ");
+            String medicationName = scanner.nextLine();
+            System.out.print("Enter updated status: ");
+            String status = scanner.nextLine();
+            ArrayList<PrescribedMedication> medlist = appointmentOutcome.getPrescribedMedicationList();
+            boolean found = false;
+            for (int i = 0; i<medlist.size(); i++){
+                PrescribedMedication med = medlist.get(i);
+                if (med.getMedicationName().equals(medicationName)){
+                    found = true;
+                    if (status.equals("dispensed")){
+                        if(inventory.updateStock(medicationName, -1)){
+                            appointmentOutcome.setMedicationStatus(med, status);
+                            System.out.println("Medication dispensed. Status updated.");
+                            break;
+                        }
+                    }
+                    else{
                         appointmentOutcome.setMedicationStatus(med, status);
-                        System.out.println("Medication dispensed. Status updated.");
+                        System.out.println("Status updated.");
                         break;
                     }
                 }
-                else{
-                    appointmentOutcome.setMedicationStatus(med, status);
-                    System.out.println("Status updated.");
-                    break;
-                }
             }
-        }
-        if (found == false){
-            System.out.println("Medication not found.");
+            if (found == false){
+                System.out.println("Medication not found.");
+            }
         }
     }
     
-    public void viewInventory(){
-        ArrayList<Medication> medlist = inventory.getMedicines();
-        if (medlist.size() == 0){
-            System.out.println("No medicine in inventory.");
-        }
-        else{
-            for (int i = 0; i<medlist.size(); i++){
-                Medication med = medlist.get(i);
-                System.out.printf("Medicine Name: %s, Quantity: %d, Low Stock Level: %d\n", med.getMedicineName(), med.getQuantity(), med.getLowStockLevel());
-            }
-        }
-        
-    }
     
-    public ArrayList<ReplenishmentRequest> requestReplenishment(String requestID, ArrayList<ReplenishmentRequest> requestList){//return all low stock medicine
-        return this.inventory.getReplenishmentRequest(requestID, requestList); 
+    public ArrayList<ReplenishmentRequest> requestReplenishment(ArrayList<ReplenishmentRequest> requestList, Inventory inventory){//return all low stock medicine
+        scanner.nextLine();
+        System.out.println("Enter RequestID: ");
+        String requestID = scanner.nextLine();
+        ArrayList<ReplenishmentRequest> replenishmentRequest = inventory.getReplenishmentRequest(requestID, requestList);
+       
+        //for sending to admin
+        //test printing///////////////////////////////////////////////////////////////////////
+        for (int i = 0; i<replenishmentRequest.size(); i++){
+            System.out.printf("%s\n", (replenishmentRequest.get(i)).getMedicine());
+        }
+        /////////////////////////////////////////////////////////////////////////////////////         
+        return inventory.getReplenishmentRequest(requestID, requestList); 
     }
 
-    public Inventory getInventory(){
-        return this.inventory;
-    }
+    // public Inventory getInventory(Inventory inven){
+    //     return inventory;
+    // }
     // public void setInventory(Inventory inventory){
 
 }
